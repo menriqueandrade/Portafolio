ddb5a13d081a3a24f3353636ec6b51e2bb81b162
Manuel Andrade <56304628+menriqueandrade@users.noreply.github.com>
2021-07-20 18:08:14 -0500

    Diseño

--- Portafilio/src/app/app.module.ts
+++ Portafilio/src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +25,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,MatIconModule,
+    MatMenuModule,MatButtonModule
+    ,MatCardModule,MatSidenavModule
+    ,MatListModule,MatDividerModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
